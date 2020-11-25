df9b8c51005628b086201a09c5a8c5a282040160
Kevin McAteer <kmcateer926@gmail.com>
2020-11-25 18:19:12 -0500

    added figlet

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ connection.connect(function(err) {
     startPrompt();
 });
 
+var figlet = require("figlet");
+
+figlet("Employee Tracker!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+
   // Initial Prompt 
 function startPrompt() {
     inquirer.prompt([
